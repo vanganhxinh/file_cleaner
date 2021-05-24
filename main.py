1207b0e34e9d2f4af1af146aca36b415cacd3ba2
Tham Van Anh <vanganh2710@gmail.com>
2021-05-24 22:20:11 +0800

    main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+
+# Function to remove unwanted files recursively in a folder
+# Input parameter 1: path to the folder - string (e.g.: "C:\\test_file_removal")
+# Input parameter 2: ending of the filename - string (e.g.: "_TN.png")
+
+
+def remove_files(folder_path, file_ending):
+    # use os.walk
+    for root, dirs, files in os.walk(folder_path, topdown=False):
+        for name in files:
+            if name.endswith(file_ending):
+                # print(f"Found [{name}] in [{root}]")
+                del_path = os.path.join(root, name)
+                print(f"  >> erase {del_path}")
+                # os.remove(del_path)
+
+
+if __name__ == '__main__':
+    remove_files("C:\\test_file_removal", "_TN.png")
